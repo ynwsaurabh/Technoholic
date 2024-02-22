553ac2ff18bc01aefe89db6f3fb6a240a37ba11c
Saurabh <ynwsaurabh@gmail.com>
2024-02-22 14:43:37 +0530

    logo added

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="/Images/techLogo.jpg"></link>
       </head>
       <body>
         {children}
